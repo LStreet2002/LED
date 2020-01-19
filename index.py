5cc8c77c401f06d1f1d50732c63dacbd8b0c676b
new-byt <55684787+new-byt@users.noreply.github.com>
2020-01-19 16:14:25 +0000

    Checkbox + will actually work fr this time

--- index.py
+++ index.py
@@ -1,29 +1,27 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
 
-
-@app.route('/', methods=['GET'])
-def index():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18, GPIO.OUT)
+@app.route('/')
+def start():  
     return render_template('index.html')
 
-
-@app.route('/led', methods=['POST'])
-def led():
-    if request.form['button'] == 'On':
-        print("on")
+@app.route('/toggle', methods=['POST'])
+def toggle():
+    if request.form.get('button'):
+        print("On")
         GPIO.output(18, GPIO.HIGH)
-        pass
-    elif request.form['button'] == 'Off':
-        print("off")
+    else:
+        print("Off")
         GPIO.output(18, GPIO.LOW)
-        pass
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Code needed in the HTML
+# -----------------------
+#<form method="post" action="/toggle">
+#	<input type="checkbox" name="button"/>
+#	<input type="submit"/>
+#</form>
